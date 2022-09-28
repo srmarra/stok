c4dd9220ee60e65f41d77098d5f1340a0f6e54ce
srmarra <social@herickmarra.com.br>
2022-09-28 10:36:29 -0300

    Update Login.js

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -57,7 +57,7 @@ function Login(){
                 <div>
                     <section>
                         <h1>STOK</h1>
-                        <p>GERENCIAMENTO DE ESTOQUE</p>
+                        <p>GERENCIAMENTO DE ESTOQUEd</p>
                     </section>
                    {/* Area Form */}
                         <section>
